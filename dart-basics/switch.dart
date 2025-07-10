809f0cf48138ce2b34dc725da41fa6e13d49a265
KTrordu <alptug.yilmaz@outlook.com>
2025-07-10 23:18:31 +0300

    added switch.dart

---
+++ dart-basics/switch.dart
@@ -0,0 +1,20 @@
+void main() {
+  int num = 3;
+
+  switch (num) {
+    case 1:
+      print('luga');
+
+    case 2:
+      print('anayuna');
+
+    case 3:
+      print(num);
+
+    case 4:
+      print(num * 5);
+
+    default:
+      print('default');
+  }
+}
